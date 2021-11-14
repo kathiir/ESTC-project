af453ad528431441cd91645a5779688e134d589e
Elizaveta Bulavina <bulavina.liza@yandex.ru>
2021-11-14 21:08:08 +0300

    close #7 implemented double click

--- blinky/main.c
+++ blinky/main.c
@@ -11,18 +11,76 @@
 #include "app_usbd.h"
 #include "app_usbd_serial_num.h"
 
+#include "nrfx_gpiote.h"
+
 #include "gpio_module.h"
 #include "gpio_pwm.h"
 
+#include "app_timer.h"
+
 #define MAX_NUMBER 6
 #define BLINK_PERIOD 600
+#define DOUBLE_CLICK_TIMEOUT (APP_TIMER_CLOCK_FREQ >> 1)
+#define DOUBLE_CLICK_MIN_TIMEOUT (APP_TIMER_CLOCK_FREQ >> 4)
 
-void logs_init()
+bool volatile smooth_blinking_on = false;
+bool volatile timed_out = true;
+
+uint32_t volatile time;
+
+APP_TIMER_DEF(timeout_timer_id);
+
+void button_evt_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+{
+    if (action == NRF_GPIOTE_POLARITY_HITOLO)
+    {
+        if (!timed_out && app_timer_cnt_diff_compute(app_timer_cnt_get(), time) < DOUBLE_CLICK_TIMEOUT && app_timer_cnt_diff_compute(app_timer_cnt_get(), time) > DOUBLE_CLICK_MIN_TIMEOUT)
+        {
+            smooth_blinking_on ^= true;
+            timed_out = true;
+        }
+        else if (app_timer_cnt_diff_compute(app_timer_cnt_get(), time) > DOUBLE_CLICK_TIMEOUT)
+        {
+            time = app_timer_cnt_get();
+            timed_out = false;
+        }
+    }
+}
+
+static void timer_timeout_handler(void *p_context)
+{
+}
+
+void logs_init(void)
 {
     ret_code_t ret = NRF_LOG_INIT(NULL);
     APP_ERROR_CHECK(ret);
 
     NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+void systick_init(void)
+{
+    nrfx_systick_init();
+}
+
+void timer_init(void)
+{
+    ret_code_t ret = app_timer_init();
+    APP_ERROR_CHECK(ret);
+
+    app_timer_create(&timeout_timer_id, APP_TIMER_MODE_REPEATED, &timer_timeout_handler);
+    app_timer_start(timeout_timer_id, 200000000, NULL);
+    time = app_timer_cnt_get();
+}
+
+void gpiote_button_init(void)
+{
+    nrfx_gpiote_init();
+    nrfx_gpiote_in_config_t config = NRFX_GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
+    config.pull = BUTTON_PULL;
+    nrfx_gpiote_in_init(BUTTON_1, &config, &button_evt_handler);
+    nrfx_gpiote_in_event_enable(BUTTON_1, true);
 }
 
 /**
@@ -53,9 +111,21 @@ int main(void)
     NRF_LOG_PROCESS();
     LOG_BACKEND_USB_PROCESS();
 
-    nrfx_systick_init();
+    systick_init();
 
     NRF_LOG_INFO("SysTick initialized");
+    NRF_LOG_PROCESS();
+    LOG_BACKEND_USB_PROCESS();
+
+    timer_init();
+
+    NRF_LOG_INFO("App timer initialized");
+    NRF_LOG_PROCESS();
+    LOG_BACKEND_USB_PROCESS();
+
+    gpiote_button_init();
+
+    NRF_LOG_INFO("GPIOTE initialized");
     NRF_LOG_PROCESS();
     LOG_BACKEND_USB_PROCESS();
 
@@ -69,38 +139,38 @@ int main(void)
     {
         LOG_BACKEND_USB_PROCESS();
 
-        if (gpio_module_button_state_get(btn_id))
+        if (blink_idx >= serial_number[led_idx])
         {
-            if (blink_idx >= serial_number[led_idx])
-            {
-                led_idx = (led_idx + 1) % LEDS_NUMBER;
-                blink_idx = 0;
-                /* 
+            led_idx = (led_idx + 1) % LEDS_NUMBER;
+            blink_idx = 0;
+            /* 
                 * Possible to do with while but it might cause infinite  
                 * loop and button state would never be read 
                 */
-                continue;
-            }
+            continue;
+        }
 
-            duty_cycle = (timer < BLINK_PERIOD / 2) 
-                        ? (timer * 200 / BLINK_PERIOD) 
-                        : (100 - (timer * 200 / BLINK_PERIOD) % 100); 
-
-            NRF_LOG_INFO("LED %d, Iteration %d, Duty cycle %d", 
-                         led_idx, blink_idx, duty_cycle);
-
-            NRF_LOG_PROCESS();
-            LOG_BACKEND_USB_PROCESS();
-
-            pwm_led_duty_cycle(led_idx, duty_cycle);
-
+        if (smooth_blinking_on)
+        {
+            duty_cycle = (timer < BLINK_PERIOD / 2)
+                             ? (timer * 200 / BLINK_PERIOD)
+                             : (100 - (timer * 200 / BLINK_PERIOD) % 100);
             timer++;
-            if (timer > BLINK_PERIOD)
-            {
-                timer = 0;
-                duty_cycle = 0;
-                blink_idx++;
-            }
+        }
+
+        NRF_LOG_INFO("LED %d, Iteration %d, Duty cycle %d",
+                     led_idx, blink_idx, duty_cycle);
+
+        NRF_LOG_FLUSH();
+        LOG_BACKEND_USB_PROCESS();
+
+        pwm_led_duty_cycle(led_idx, duty_cycle);
+
+        if (timer > BLINK_PERIOD)
+        {
+            timer = 0;
+            duty_cycle = 0;
+            blink_idx++;
         }
 
         LOG_BACKEND_USB_PROCESS();
